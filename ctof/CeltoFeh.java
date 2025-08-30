f115ff7d7efc13ee9404ef86f6dd1c4b06df2e73
Shriya-26-Sharma <shriyasharma0126@gmail.com>
2025-08-30 22:42:35 +0530

    Celsius to Fahrenheit

--- ctof/CeltoFeh.java
+++ ctof/CeltoFeh.java
@@ -4,7 +4,7 @@ public class CeltoFeh {
 public static void main(String args[])
 {
 Scanner sc=new Scanner(System.in);
-System.out.println("Enter Celcius");
+System.out.println("Enter Celsius");
 double cel=sc.nextDouble();
 double feh=32+(9*cel/5);
 System.out.println("Fahrenheit : "+ feh );
