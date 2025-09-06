3245fe91255c294f8c4a779109d9689a6a498251
Shriya-26-Sharma <shriyasharma0126@gmail.com>
2025-09-06 22:38:00 +0530

    add funct1

---
+++ parameter.java
@@ -0,0 +1,12 @@
+class parameter {
+    public static void main(String[] args) {
+        int num =sumTwo(10,20);
+        System.out.println(num);
+    }
+
+    public static int sumTwo(int first,int second){
+
+       return(first+second);
+
+    }
+}
