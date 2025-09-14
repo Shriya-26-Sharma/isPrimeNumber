42589c039977b94a27575dbff68330b3fbea1444
Shriya-26-Sharma <shriyasharma0126@gmail.com>
2025-09-14 23:06:30 +0530

    Find the Number Present in 2d array

--- arrayInput.java
+++ arrayInput.java
@@ -1,22 +1,31 @@
 import java.util.Scanner;
 public class arrayInput {
-   public static void main(String[] args) {
-      
+   public static void main(String[] args) {    
    }
-   public static int[] inputArray() {
+   public static int[][] inputArray() {
       Scanner var0 = new Scanner(System.in);
-      System.out.println("Enter the size ");
-      int var1 = var0.nextInt();
-      int[] var2 = new int[var1];
-      for(int var3 = 0; var3 < var1; ++var3) {
-         System.out.println("Enter the " + var3 + " th number");
-         var2[var3] = var0.nextInt();
-      }
-      return var2;
-   }
-   public static int [] displayArray(int var2 []){
+      System.out.println("Enter the row ");
+      int row = var0.nextInt();
+        System.out.println("Enter the column ");
+      int col = var0.nextInt();
+      int[] []var2 = new int[row][col];
+      int i=0;
+      while(i<row){
+         int j=0;
+         while(j<col){
+          System.out.println("Enter the row :"+(i+1) +" th number And Column :"+(j+1));
+          var2[i][j] = var0.nextInt();
+            j++;
+          }
+           i++;}
+          return var2;
+          }
+   public static int [][] displayArray(int var2 [][]){
     for(int i = 0;i<var2.length;i++){
-         System.out.println(var2[i]+" ");
+      for(int j=0;j<var2.length;j++){
+          System.out.print(var2[i][j]+" ");
+      }
+      System.out.println(" ");
     }
     return var2;
 }
