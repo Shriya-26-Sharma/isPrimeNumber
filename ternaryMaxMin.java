daf9b407285f62b9fda31384f336599e7b5c2cb9
Shriya-26-Sharma <shriyasharma0126@gmail.com>
2025-09-23 22:17:06 +0530

    Ternary operator Max Min

---
+++ ternaryMaxMin.java
@@ -0,0 +1,11 @@
+ import java.util.*;
+ class ternaryMaxMin {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the numbers");
+        int num1=sc.nextInt();
+        int num2=sc.nextInt();
+        int greatestNum=num1>num2?num1:num2;
+        System.out.println("The greatest is :"+greatestNum);
+    }
+}
