67fdc3e59b5046139144ca63bdc327847b7968ce
Shriya-26-Sharma <shriyasharma0126@gmail.com>
2025-09-26 20:22:24 +0530

    Delete colortext.java

--- colortext.java
+++
@@ -1,8 +0,0 @@
-public class colortext {
-    public static void main(String[] args) {
-        String RESET = "\u001B[0m";
-        String RED_TEXT = "\u001B[31m";
-        String BLUE_BG = "\u001B[44m";
-        System.out.println(RED_TEXT + BLUE_BG + "Hwllll." + RESET);
-    }
-}
